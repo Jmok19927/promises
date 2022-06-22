6cb552858178f7ea1066b856cd7695e1cdde39a6
Jmok19927 <jmok19927@gmail.com>
2022-06-22 10:11:42 -0700

    Complete bare_minimum/callbackReview.js exercises

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,27 @@ var fs = require('fs');
 var request = require('needle');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, callback) {
   // TODO
+  fs.readFile(filePath, 'utf8', (err, text) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      var textArray = text.split('\n');
+      callback(null, textArray[0]);
+    }
+  })
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  request.get(url, (err, resp) => {
+    if (err) {
+      callback(err, null);
+   } else {
+    callback(null, resp.statusCode);
+   }
+  })
 };
 
 // Export these functions so we can test them and reuse them in later exercises
